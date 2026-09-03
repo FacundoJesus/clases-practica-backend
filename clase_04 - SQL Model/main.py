652c002286b658c1dd3c7f2e5108437587eade55
Facundo Citera <facundojesus10@hotmail.com>
2026-09-02 21:14:13 -0300

    ...

--- clase_04 - SQL Model/main.py
+++ clase_04 - SQL Model/main.py
@@ -1,8 +1,12 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, create_engine
 
 # Se define el modelo
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
 
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}";
 
+connect_args = {"check_same_thread": False}  # Solo para SQLite
+engine = create_engine(sqlite_url, connect_args=connect_args)
